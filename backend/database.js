d61f40bde34fa72e4ff239a3176dd7f03e9530a3
Virgo Aries <lenterakaryasitubondo@gmail.com>
2025-08-02 15:02:39 +0700

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,1 +1,12 @@
-.
+// Simulated database functions
+class Database {
+    static get(key) {
+        return JSON.parse(localStorage.getItem(key)) || [];
+    }
+
+    static set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+}
+
+module.exports = Database;
